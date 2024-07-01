27c92e7b7d028d495388cf72e42fdec697a971b2
Vitaliy Sulimenko <prevetal@gmail.com>
2024-07-01 20:08:47 +0400

    Update import

--- src/views/ImportWalletPage.vue
+++ src/views/ImportWalletPage.vue
@@ -33,99 +33,99 @@
                     <div class="mnemonic" v-if="activeTab != 3">
                         <div class="row">
                             <div class="word">
-                                <input type="text" class="input" v-model="secret[0]" @input="validateWord" @paste="handlePaste">
+                                <input type="text" class="input" v-model="secret[0]" @input="validateWord($event.target.value, 0)" @paste="handlePaste" :class="getValidationClass(0)">
                             </div>
 
                             <div class="word">
-                                <input type="text" class="input" v-model="secret[1]" @input="validateWord" @paste="handlePaste">
+                                <input type="text" class="input" v-model="secret[1]" @input="validateWord($event.target.value, 1)" @paste="handlePaste" :class="getValidationClass(1)">
                             </div>
 
                             <div class="word">
-                                <input type="text" class="input" v-model="secret[2]" @input="validateWord" @paste="handlePaste">
+                                <input type="text" class="input" v-model="secret[2]" @input="validateWord($event.target.value, 2)" @paste="handlePaste" :class="getValidationClass(2)">
                             </div>
 
                             <div class="word">
-                                <input type="text" class="input" v-model="secret[3]" @input="validateWord" @paste="handlePaste">
+                                <input type="text" class="input" v-model="secret[3]" @input="validateWord($event.target.value, 3)" @paste="handlePaste" :class="getValidationClass(3)">
                             </div>
 
                             <div class="word">
-                                <input type="text" class="input" v-model="secret[4]" @input="validateWord" @paste="handlePaste">
+                                <input type="text" class="input" v-model="secret[4]" @input="validateWord($event.target.value, 4)" @paste="handlePaste" :class="getValidationClass(4)">
                             </div>
 
                             <div class="word">
-                                <input type="text" class="input" v-model="secret[5]" @input="validateWord" @paste="handlePaste">
+                                <input type="text" class="input" v-model="secret[5]" @input="validateWord($event.target.value, 5)" @paste="handlePaste" :class="getValidationClass(5)">
                             </div>
 
                             <div class="word">
-                                <input type="text" class="input" v-model="secret[6]" @input="validateWord" @paste="handlePaste">
+                                <input type="text" class="input" v-model="secret[6]" @input="validateWord($event.target.value, 6)" @paste="handlePaste" :class="getValidationClass(6)">
                             </div>
 
                             <div class="word">
-                                <input type="text" class="input" v-model="secret[7]" @input="validateWord" @paste="handlePaste">
+                                <input type="text" class="input" v-model="secret[7]" @input="validateWord($event.target.value, 7)" @paste="handlePaste" :class="getValidationClass(7)">
                             </div>
 
                             <div class="word">
-                                <input type="text" class="input" v-model="secret[8]" @input="validateWord" @paste="handlePaste">
+                                <input type="text" class="input" v-model="secret[8]" @input="validateWord($event.target.value, 8)" @paste="handlePaste" :class="getValidationClass(8)">
                             </div>
 
                             <div class="word">
-                                <input type="text" class="input" v-model="secret[9]" @input="validateWord" @paste="handlePaste">
+                                <input type="text" class="input" v-model="secret[9]" @input="validateWord($event.target.value, 9)" @paste="handlePaste" :class="getValidationClass(9)">
                             </div>
 
                             <div class="word">
-                                <input type="text" class="input" v-model="secret[10]" @input="validateWord" @paste="handlePaste">
+                                <input type="text" class="input" v-model="secret[10]" @input="validateWord($event.target.value, 10)" @paste="handlePaste" :class="getValidationClass(10)">
                             </div>
 
                             <div class="word">
-                                <input type="text" class="input" v-model="secret[11]" @input="validateWord" @paste="handlePaste">
+                                <input type="text" class="input" v-model="secret[11]" @input="validateWord($event.target.value, 11)" @paste="handlePaste" :class="getValidationClass(11)">
                             </div>
 
                             <div class="word" v-if="activeTab === 2">
-                                <input type="text" class="input" v-model="secret[12]" @input="validateWord" @paste="handlePaste">
+                                <input type="text" class="input" v-model="secret[12]" @input="validateWord($event.target.value, 12)" @paste="handlePaste" :class="getValidationClass(12)">
                             </div>
 
                             <div class="word" v-if="activeTab === 2">
-                                <input type="text" class="input" v-model="secret[13]" @input="validateWord" @paste="handlePaste">
+                                <input type="text" class="input" v-model="secret[13]" @input="validateWord($event.target.value, 13)" @paste="handlePaste" :class="getValidationClass(13)">
                             </div>
 
                             <div class="word" v-if="activeTab === 2">
-                                <input type="text" class="input" v-model="secret[14]" @input="validateWord" @paste="handlePaste">
+                                <input type="text" class="input" v-model="secret[14]" @input="validateWord($event.target.value, 14)" @paste="handlePaste" :class="getValidationClass(14)">
                             </div>
 
                             <div class="word" v-if="activeTab === 2">
-                                <input type="text" class="input" v-model="secret[15]" @input="validateWord" @paste="handlePaste">
+                                <input type="text" class="input" v-model="secret[15]" @input="validateWord($event.target.value, 15)" @paste="handlePaste" :class="getValidationClass(15)">
                             </div>
 
                             <div class="word" v-if="activeTab === 2">
-                                <input type="text" class="input" v-model="secret[16]" @input="validateWord" @paste="handlePaste">
+                                <input type="text" class="input" v-model="secret[16]" @input="validateWord($event.target.value, 16)" @paste="handlePaste" :class="getValidationClass(16)">
                             </div>
 
                             <div class="word" v-if="activeTab === 2">
-                                <input type="text" class="input" v-model="secret[17]" @input="validateWord" @paste="handlePaste">
+                                <input type="text" class="input" v-model="secret[17]" @input="validateWord($event.target.value, 17)" @paste="handlePaste" :class="getValidationClass(17)">
                             </div>
 
                             <div class="word" v-if="activeTab === 2">
-                                <input type="text" class="input" v-model="secret[18]" @input="validateWord" @paste="handlePaste">
+                                <input type="text" class="input" v-model="secret[18]" @input="validateWord($event.target.value, 18)" @paste="handlePaste" :class="getValidationClass(18)">
                             </div>
 
                             <div class="word" v-if="activeTab === 2">
-                                <input type="text" class="input" v-model="secret[19]" @input="validateWord" @paste="handlePaste">
+                                <input type="text" class="input" v-model="secret[19]" @input="validateWord($event.target.value, 19)" @paste="handlePaste" :class="getValidationClass(19)">
                             </div>
 
                             <div class="word" v-if="activeTab === 2">
-                                <input type="text" class="input" v-model="secret[20]" @input="validateWord" @paste="handlePaste">
+                                <input type="text" class="input" v-model="secret[20]" @input="validateWord($event.target.value, 20)" @paste="handlePaste" :class="getValidationClass(20)">
                             </div>
 
                             <div class="word" v-if="activeTab === 2">
-                                <input type="text" class="input" v-model="secret[21]" @input="validateWord" @paste="handlePaste">
+                                <input type="text" class="input" v-model="secret[21]" @input="validateWord($event.target.value, 21)" @paste="handlePaste" :class="getValidationClass(21)">
                             </div>
 
                             <div class="word" v-if="activeTab === 2">
-                                <input type="text" class="input" v-model="secret[22]" @input="validateWord" @paste="handlePaste">
+                                <input type="text" class="input" v-model="secret[22]" @input="validateWord($event.target.value, 22)" @paste="handlePaste" :class="getValidationClass(22)">
                             </div>
 
                             <div class="word" v-if="activeTab === 2">
-                                <input type="text" class="input" v-model="secret[23]" @input="validateWord" @paste="handlePaste">
+                                <input type="text" class="input" v-model="secret[23]" @input="validateWord($event.target.value, 23)" @paste="handlePaste" :class="getValidationClass(23)">
                             </div>
                         </div>
 
@@ -175,6 +175,7 @@
         activeTab = ref(1),
         wallet = ref(null),
         secret = ref([]),
+        validateAllWordsResult = ref([]),
         privateKey = ref(''),
         idValidPrivateKey = ref(false),
         isTouchedPrivateKey = ref(false)
@@ -189,6 +190,7 @@
     watch(activeTab, () => {
         // Reset data
         secret.value = []
+        validateAllWordsResult.value = []
         privateKey.value = ''
         isTouchedPrivateKey.value = false
     })
@@ -207,6 +209,9 @@
 
             // Set secret
             setTimeout(() => secret.value = pastedTextArr)
+
+            // Validate
+            setTimeout(() => validateAllWords())
         }
 
         if (pastedTextArr.length == 24) {
@@ -215,33 +220,58 @@
 
             // Set secret
             setTimeout(() => secret.value = pastedTextArr)
+
+            // Validate
+            setTimeout(() => validateAllWords())
         }
     }
 
 
+    // Validate all words
+    function validateAllWords() {
+        secret.value.map((value, i) => validateWord(value, i))
+    }
+
+
     // Validate word
-    function validateWord(e) {
-        let result = []
+    function validateWord(str, i) {
+        let result = true
 
         // Validate length
-        e.target.value.trim().length
-            ? result.push(true)
-            : result.push(false)
+        if (!str.trim().length) {
+            result = false
+        }
 
         // Validate symbols
         let regex = /^[a-z]+$/
 
-        result.push(regex.test(e.target.value.trim()))
+        if (!regex.test(str.trim())) {
+            result = false
+        }
+
+        validateAllWordsResult.value[i] = result
 
 
         // Set class
-        if (result.every(value => value === true)) {
-            e.target.classList.remove('error')
-            e.target.classList.add('success')
-        } else {
-            e.target.classList.remove('success')
-            e.target.classList.add('error')
+        // if (result.every(value => value === true)) {
+        //     e.target.classList.remove('error')
+        //     e.target.classList.add('success')
+        // } else {
+        //     e.target.classList.remove('success')
+        //     e.target.classList.add('error')
+        // }
+    }
+
+
+    // Get validate class
+    function getValidationClass(i) {
+        let result = validateAllWordsResult.value[i]
+
+        if (result === null || result === undefined) {
+            return ''
         }
+
+        return result ? 'success' : 'error'
     }
 
 
