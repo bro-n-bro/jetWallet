eb381048c33e218e75b295ff330a5409b837120a
prevetal <31705823+prevetal@users.noreply.github.com>
2025-01-25 12:13:24 +0000

    Deploying to build from @ bro-n-bro/jetWallet@dfbd971e9012759dd26ff1fdb794f5bb222beabb 🚀

---
+++ js/71.061408fe.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkcosmos_telegram_signer"]=self["webpackChunkcosmos_telegram_signer"]||[]).push([[71],{7078:function(e,n,r){r.r(n),r.d(n,{default:function(){return c}});var s=r(56768),t=r(32077),u={__name:"ConfirmPage",setup(e){return(e,n)=>((0,s.uX)(),(0,s.Wv)(t.A,{isAdding:"true"}))}};const a=u;var c=a}}]);
+//# sourceMappingURL=71.061408fe.js.map
